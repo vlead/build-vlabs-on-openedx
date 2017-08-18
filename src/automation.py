dcd8feeae99f1b171d1374b0e7ad4006fdccc964
SravanthiModepu <sravanthimodepu@vlabs.ac.in>
2017-08-18 11:50:47 +0530

    modified the script

--- src/automation.py
+++ src/automation.py
@@ -1,15 +1,26 @@
 import json
+import sys
+import shutil
 from pprint import pprint
 from git import Repo
-import sys
+import tarfile
+import os
+
    
 with open('labs.json') as data_file:    
     data = json.load(data_file)
-    for i in range(5):
+    for i in range(2):
         repo_name = (data[i]["name"].encode("utf-8"))
         repo = (data[i]["html_url"].encode("utf-8"))
         
-#        print("/home/sravanthi/Music/automation-labs/" + repo_name)
-        Repo.clone_from(repo, "/home/sravanthi/Music/automation-labs/" + repo_name)
-#Repo.clone_from("https://github.com/vlabs-on-openedx/open-edx-computer-programming-iiith",
-#                         "/home/sravanthi/Music/automation-labs/computer-iiith")
+       # print("/home/sravanthi/Videos/automation-labs/" + repo_name)
+        Repo.clone_from(repo, "/home/sravanthi/Videos/automation-labs/" + repo_name)
+
+ #       shutil.make_archive(i.zip, 'zip', vlabs-automation-labs)
+
+ 
+#folder_path = ("/home/sravanthi/Videos/automation-labs/" + repo_name) 
+with tarfile.open(  (data[i]["name"].encode("utf-8")) + ".tar.gz", "w:gz" ) as tar:
+    for name in os.listdir("/home/sravanthi/Videos/automation-labs/")
+    for name in shutil.move( "/home/sravanthi/Videos/build-vlabs-on-openedx/*", "/home/sravanthi/Videos/automation-labs-tar/" ):
+        tar.add(name)
